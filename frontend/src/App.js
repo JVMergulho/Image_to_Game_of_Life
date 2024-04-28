fc69ec5da372db2ee42cc62ddf189a6916508283
João Vitor Mergulhão <jvitormergulho@gmail.com>
2024-04-28 19:11:46 -0300

    feat: add form for sending image to backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,39 @@
-import * as React from "react";
+import React, { useState } from "react";
 import { ReactP5Wrapper } from "@p5-wrapper/react";
 import sketch from "./sketches/sketch"
 
+const API_BASE = "http://localhost:4000"
+
 export default function App() {
-  return <ReactP5Wrapper sketch={sketch} />;
+
+  const [imageFile, setImageFile] = useState('')
+
+  async function uploadImage(ev){
+    ev.preventDefault()
+
+    const data = new FormData();
+    data.append('file', imageFile[0]);
+  
+    const response = await fetch(API_BASE + '/upload', {
+      method: 'POST',
+      body: data
+    })
+
+    console.log(response)
+  }
+
+  return (
+  <div>
+    <ReactP5Wrapper sketch={sketch} />
+    <form>
+      <div className="form-field">
+          <label htmlFor="uploadImage">Faça o upload de uma imagem</label>
+          <input type="file" id="uploadImage" name="imageFile"
+          onChange={ev => setImageFile(ev.target.files)}/>
+      </div>
+      <button className="simple-button" id="create-button" onClick={(uploadImage)}>
+          <p>Enviar</p>
+      </button>
+    </form>
+  </div>)
 }
